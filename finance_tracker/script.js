6ea3a2f32be742aaef8cf5954046a9a8f0b5bef2
SaracensFan <matthewclifford@hotmail.co.uk>
2022-02-22 23:25:27 +0000

    updates@

--- finance_tracker/script.js
+++ finance_tracker/script.js
@@ -1,57 +1,113 @@
 const paymentType = document.getElementById('paymentType')
 const amount = document.getElementById('amount')
 const category = document.getElementById('category')
+const company = document.getElementById('company')
 const date = document.getElementById('date')
 const submit = document.getElementById('submit')
 const outputContainer = document.getElementById('outputContainer')
 const totalSpent = document.getElementById('totalSpent')
+const totalDebit = document.getElementById('totalDebit')
+const totalCredit = document.getElementById('totalCredit')
 
-let total = 0
+let spent = 0
+let debit = 0
+let credit = 0
+let items = localStorage.getItem('data') ? JSON.parse(localStorage.getItem('data')) : []
 
 submit.addEventListener('click', process)
 
+
+
 function process() {
-    errorMessages()
-    addNewItem()
-    console.log(total);
-}
-
-function addNewItem() {
-    total += parseInt(amount.value)
-    totalSpent.innerText = `$${total}` 
+    createArray()
     createNewItem()
-    }
-
-function createNewItem() {
-    const divNewItem = document.createElement('div')
-    const pNewAmount = document.createElement('div')
-    const pNewType = document.createElement('div')
-    const pNewCat = document.createElement('div')
-    const pNewDate = document.createElement('div')
-
-    divNewItem.classList.add('new-item')
-
-    pNewAmount.innerText = `$${amount.value}`
-    pNewType.innerText = paymentType.value
-    pNewCat.innerText = category.value
-    pNewDate.innerText = date.value
-
-    divNewItem.appendChild(pNewAmount)
-    divNewItem.appendChild(pNewType)
-    divNewItem.appendChild(pNewCat)
-    divNewItem.appendChild(pNewDate)
-
-    outputContainer.appendChild(divNewItem)
 }
 
-function errorMessages() {
-    // if (paymentType.value === '') { /*FIX ISSUE*/
-    //   alert('Enter Type') } 
-    if (amount.value === '') {
-        alert('Enter amount')
-    } else if (category.value === '') {
-        alert('Enter category')
-    } else if (date.value === '') {
-        alert('Enter date')
+items.forEach(createNewItem)
+
+function createNewItem(text) {
+ 
+        const divNewItem = document.createElement('div')
+        const pNewAmount = document.createElement('div')
+        const pNewType = document.createElement('div')
+        const pNewCat = document.createElement('div')
+        const pNewCom = document.createElement('div')
+        const pNewDate = document.createElement('div')
+
+        divNewItem.classList.add('new-item')
+        // FIX CREDIT/ DEBIT COLOR - because array is creating object
+        if(paymentType.value === 'DEBIT') {
+            pNewAmount.innerText = `-$${text.amount}`
+            pNewAmount.style.color = 'var(--txt-three)'
+        } else {
+            pNewAmount.innerText = `+$${text.amount}`
+            pNewAmount.style.color = 'var(--txt-two)'
+        }
+        pNewType.innerText = text.type
+        pNewCat.innerText = text.category
+        pNewCom.innerText = text.company
+        pNewDate.innerText = text.date
+
+        divNewItem.appendChild(pNewAmount)
+        divNewItem.appendChild(pNewType)
+        divNewItem.appendChild(pNewCat)
+        divNewItem.appendChild(pNewCom)
+        divNewItem.appendChild(pNewDate)
+
+        outputContainer.appendChild(divNewItem)
+
+        addNewItem()
+    
+}
+
+// FIX TOTAL SPENT HEADINGS
+function addNewItem(text) {
+    if (paymentType.value === 'DEBIT') {
+        spent += parseFloat(amount.value)
+        debit += parseFloat(amount.value)
+        totalSpent.innerText = `$${spent}`
+        totalDebit.innerText = `-$${debit}`
+        totalDebit.style.color = 'var(--txt-three)'
+    } else {
+        spent -= parseFloat(amount.value)
+        credit += parseFloat(amount.value)
+        totalSpent.innerText = `$${spent}`
+        totalCredit.innerText = `+$${credit}`
+        totalCredit.style.color = 'var(--txt-two)'
     }
 }
+
+function createArray() {
+    let itemObj = {
+        'type': paymentType.value,
+        'amount': amount.value, 
+        'category': category.value,
+        'company': company.value,
+        'date': date.value
+    }
+    items.push(itemObj)
+    localStorage.setItem('data', JSON.stringify(items))
+}
+
+
+function todayDate() {
+    const todayDate = new Date()
+    let day = todayDate.getDay()
+    let month = todayDate.getMonth() + 1
+    let year = todayDate.getFullYear()
+    day = addZero(day)
+    month = addZero(month)
+
+    function addZero(x) {
+        if (x < 10) {
+            x = '0' + x
+        }
+        return x
+    }
+    date.value = year + '-' + month + '-' + day
+}
+
+todayDate()
+
+// REMOVE ITEM FUNCTION
+// LOCAL STORAGE
