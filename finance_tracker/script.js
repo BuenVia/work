97f465d93648c40a843e43633b0b1d21ef7a9c29
SaracensFan <matthewclifford@hotmail.co.uk>
2022-02-23 08:26:59 +0000

    update

--- finance_tracker/script.js
+++ finance_tracker/script.js
@@ -4,26 +4,37 @@ const category = document.getElementById('category')
 const company = document.getElementById('company')
 const date = document.getElementById('date')
 const submit = document.getElementById('submit')
+const clear = document.getElementById('clear')
 const outputContainer = document.getElementById('outputContainer')
 const totalSpent = document.getElementById('totalSpent')
 const totalDebit = document.getElementById('totalDebit')
 const totalCredit = document.getElementById('totalCredit')
 
-let spent = 0
-let debit = 0
-let credit = 0
 let items = localStorage.getItem('data') ? JSON.parse(localStorage.getItem('data')) : []
 
+
 submit.addEventListener('click', process)
+clear.addEventListener('click', clearItems)
 
-
+items.forEach(createNewItem)
 
 function process() {
     createArray()
-    createNewItem()
+    calcSpent()
 }
 
-items.forEach(createNewItem)
+function createArray() {
+    let itemObj = {
+        'amount': amount.value, 
+        'type': paymentType.value,
+        'category': category.value,
+        'company': company.value,
+        'date': date.value
+    }
+    items.push(itemObj)
+    localStorage.setItem('data', JSON.stringify(items))
+    createNewItem(items[items.length - 1])
+}
 
 function createNewItem(text) {
  
@@ -36,7 +47,7 @@ function createNewItem(text) {
 
         divNewItem.classList.add('new-item')
         // FIX CREDIT/ DEBIT COLOR - because array is creating object
-        if(paymentType.value === 'DEBIT') {
+        if(text.type === 'DEBIT') {
             pNewAmount.innerText = `-$${text.amount}`
             pNewAmount.style.color = 'var(--txt-three)'
         } else {
@@ -54,41 +65,37 @@ function createNewItem(text) {
         divNewItem.appendChild(pNewCom)
         divNewItem.appendChild(pNewDate)
 
-        outputContainer.appendChild(divNewItem)
+        outputContainer.appendChild(divNewItem)  
 
-        addNewItem()
-    
 }
 
-// FIX TOTAL SPENT HEADINGS
-function addNewItem(text) {
-    if (paymentType.value === 'DEBIT') {
-        spent += parseFloat(amount.value)
-        debit += parseFloat(amount.value)
-        totalSpent.innerText = `$${spent}`
-        totalDebit.innerText = `-$${debit}`
-        totalDebit.style.color = 'var(--txt-three)'
-    } else {
-        spent -= parseFloat(amount.value)
-        credit += parseFloat(amount.value)
-        totalSpent.innerText = `$${spent}`
-        totalCredit.innerText = `+$${credit}`
-        totalCredit.style.color = 'var(--txt-two)'
+function calcSpent() {
+    let debit = 0
+    let credit = 0
+    let spent = 0
+    for (let i = 0; i < items.length; i++) {
+        if (items[i].type === 'DEBIT') {
+            debit += parseFloat(items[i].amount)
+        } else if (items[i].type === 'CREDIT') {
+            credit += parseFloat(items[i].amount)
+        }
     }
-}
 
-function createArray() {
-    let itemObj = {
-        'type': paymentType.value,
-        'amount': amount.value, 
-        'category': category.value,
-        'company': company.value,
-        'date': date.value
-    }
-    items.push(itemObj)
-    localStorage.setItem('data', JSON.stringify(items))
-}
+    spent = (debit - credit)
 
+    totalSpent.innerText = `$${spent}`
+    totalDebit.innerText = `-$${debit}`
+    totalDebit.style.color = 'var(--txt-three)'
+    totalCredit.innerText = `+$${credit}`
+    totalCredit.style.color = 'var(--txt-two)'
+    console.log(spent, debit, credit);
+}
+calcSpent()
+
+function clearItems() {
+    localStorage.clear()
+    outputContainer.innerHTML = ''
+}
 
 function todayDate() {
     const todayDate = new Date()
@@ -110,4 +117,4 @@ function todayDate() {
 todayDate()
 
 // REMOVE ITEM FUNCTION
-// LOCAL STORAGE
+// ERROR MESSAGE
