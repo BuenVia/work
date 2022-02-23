4a8d96b58529d5c0bf1a153130ff7b43a1d1181d
SaracensFan <matthewclifford@hotmail.co.uk>
2022-02-23 21:59:45 +0000

    update

--- flashcard/script.js
+++ flashcard/script.js
@@ -1,17 +1,54 @@
-const card = document.getElementById('card')
-const word = document.getElementById('word')
+const showBtn = document.getElementById('show')
+const deleteBtn = document.getElementById('delete')
+const saveBtn = document.getElementById('save')
+const closeBtn = document.getElementById('close')
+const question = document.getElementById('question')
 const answer = document.getElementById('answer')
-const next = document.getElementById('next')
-const prev = document.getElementById('prev')
+const flashcards = document.getElementById('flashcards')
 
-card.addEventListener('click', reveal)
+let words = localStorage.getItem('data') ? JSON.parse(localStorage.getItem('data')) : []
 
-function reveal() {
-    if (word.style.display == 'none') {
-        word.style.display = 'block'
-        answer.style.display = 'none'
-    } else {
-        word.style.display = 'none';
-        answer.style.display = 'block'
-    }
+saveBtn.addEventListener('click', createWord)
+
+words.forEach(createFlashcard);
+// CREATE FLASHCARD
+function createFlashcard(text) {
+    let div = document.createElement('div')
+    let p_question = document.createElement('p')
+    let p_answer = document.createElement('p')
+
+    div.classList = 'flashcard'
+
+    p_question.innerHTML = text.question
+    p_answer.innerHTML = text.answer
+
+    p_question.setAttribute('style', 'text-align: center; border-bottom: 1px solid red; padding: 15px; margin-top: 30px')
+    p_answer.setAttribute('style', 'text-align: center; display: none; color: red; margin-top: 30px')
+
+    div.appendChild(p_question)
+    div.appendChild(p_answer)
+
+    div.addEventListener('click', () => {
+        p_answer.style.display === 'none' ? p_answer.style.display = 'block' : p_answer.style.display = 'none'
+    })
+
+    flashcards.appendChild(div)
 }
+
+// ADD TO ARRAY
+function createWord() {
+    let wordObj = {
+        'question': question.value,
+        'answer': answer.value
+    }
+    words.push(wordObj)
+    localStorage.setItem('data', JSON.stringify(words))
+    createFlashcard(words[words.length - 1])
+    question.value = ''
+    answer.value = ''
+}
+
+
+// DELETE FLASHCARDS
+
+// SHOW / HIDE CREATE BOX
