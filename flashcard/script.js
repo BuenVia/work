445c96727cb8d34344c44813d6f92faf58d0fe90
SaracensFan <matthewclifford@hotmail.co.uk>
2022-02-24 23:27:40 +0000

    update

--- flashcard/script.js
+++ flashcard/script.js
@@ -1,52 +1,96 @@
-const showBtn = document.getElementById('show')
-const deleteBtn = document.getElementById('delete')
+import { ser } from "./wordbank.js"
+
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
 const saveBtn = document.getElementById('save')
 const closeBtn = document.getElementById('close')
 const question = document.getElementById('question')
 const answer = document.getElementById('answer')
-const flashcards = document.getElementById('flashcards')
 
-let words = localStorage.getItem('flashcard-word') ? JSON.parse(localStorage.getItem('flashcard-word')) : []
 
-saveBtn.addEventListener('click', createWord)
+const flashcard = document.getElementById('flashcard')
+const quest = document.getElementById('quest')
+const ans = document.getElementById('ans')
 
-words.forEach(createFlashcard);
+
+next.addEventListener('click', showWord)
+
+// let words = localStorage.getItem('flashcard-word') ? JSON.parse(localStorage.getItem('flashcard-word')) : []
+
+// saveBtn.addEventListener('click', createWord)
+
+// ser.forEach(createFlashcard);
 // CREATE FLASHCARD
-function createFlashcard(text) {
-    let div = document.createElement('div')
-    let p_question = document.createElement('p')
-    let p_answer = document.createElement('p')
+// function createFlashcard(text) {
+//     let div = document.createElement('div')
+//     let p_question = document.createElement('p')
+//     let p_answer = document.createElement('p')
 
-    div.classList = 'flashcard'
+//     div.classList = 'flashcard'
 
-    p_question.innerHTML = text.question
-    p_answer.innerHTML = text.answer
+//     p_question.innerHTML = text.question
+//     p_answer.innerHTML = text.answer
 
-    p_question.setAttribute('style', 'text-align: center; border-bottom: 1px solid red; padding: 15px; margin-top: 30px')
-    p_answer.setAttribute('style', 'text-align: center; display: none; color: red; margin-top: 30px')
+//     p_question.setAttribute('style', 'text-align: center; border-bottom: 1px solid red; padding: 15px; margin-top: 30px')
+//     p_answer.setAttribute('style', 'text-align: center; display: none; color: red; margin-top: 30px')
 
-    div.appendChild(p_question)
-    div.appendChild(p_answer)
+//     div.appendChild(p_question)
+//     div.appendChild(p_answer)
 
-    div.addEventListener('click', () => {
-        p_answer.style.display === 'none' ? p_answer.style.display = 'block' : p_answer.style.display = 'none'
-    })
+//     div.addEventListener('click', () => {
+//         p_answer.style.display === 'none' ? p_answer.style.display = 'block' : p_answer.style.display = 'none'
+//     })
 
-    flashcards.appendChild(div)
+//     flashcards.appendChild(div)
+// }
+
+function createRandom(array) {
+    let currentIndex = array.length, randomIndex
+    while (currentIndex != 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex)
+        currentIndex--
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]]
+    }
+    return array
 }
+
+
+createRandom(ser)
+
+let y = 0
+
+function showWord() {
+    if (y < ser.length) {
+        quest.innerText = ser[y].question;
+        ans.innerText = ser[y].answer
+        flashcard.addEventListener('click', hideAnswer)
+        y++
+    }   else {
+        quest.innerText = ''
+    }
+} 
+
+function hideAnswer() {
+    ans.style.display === 'none' ? ans.style.display = 'block' : ans.style.display = 'none'
+}
+
+
+
+// ser.forEach(createFlashcard)
+
 
 // ADD TO ARRAY
-function createWord() {
-    let wordObj = {
-        'question': question.value,
-        'answer': answer.value
-    }
-    words.push(wordObj)
-    localStorage.setItem('flashcard-word', JSON.stringify(words))
-    createFlashcard(words[words.length - 1])
-    question.value = ''
-    answer.value = ''
-}
+// function createWord() {
+//     let wordObj = {
+//         'question': question.value,
+//         'answer': answer.value
+//     }
+//     words.push(wordObj)
+//     localStorage.setItem('flashcard-word', JSON.stringify(words))
+//     createFlashcard(words[words.length - 1])
+//     question.value = ''
+//     answer.value = ''
+// }
 
 
 // DELETE FLASHCARDS
---
+++ flashcard/wordbank.js
@@ -0,0 +1,26 @@
+export const ser = [
+{
+    'question': 'I am',
+    'answer': 'soy'
+},
+{
+    'question': 'You are (informal)',
+    'answer': 'eres'
+},
+{
+    'question': 'He/ She/ It/ Is ... [f] You are',
+    'answer': 'es'
+},
+{
+    'question': 'We are',
+    'answer': 'somos'
+},
+{
+    'question': 'You (all) are',
+    'answer': 'sois'
+},
+{
+    'question': 'They are... [F] You (all) are',
+    'answer': 'son'
+}
+]
