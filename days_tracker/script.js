ebdabb07f47300af11d5114453ac636e7938da3c
Matthew Clifford <matthewclifford@hotmail.co.uk>
2022-03-02 15:12:15 +0000

    update

---
+++ days_tracker/script.js
@@ -0,0 +1,25 @@
+const date = document.getElementById('date')
+const submitBtn = document.getElementById('submit')
+
+const today = new Date();
+const todayYear = today.getFullYear()
+let todayMonth = today.getMonth()
+let todayDay = today.getDay() - 1
+todayMonth = addZero(todayMonth)
+todayDay = addZero(todayDay)
+
+const now = `${todayYear}-${todayMonth}-${todayDay}`
+
+
+
+function addZero(item) {
+    if (item <= 9) {
+        item = "0" + item
+        return item
+    }
+}
+
+
+submitBtn.addEventListener('click', () => {
+    console.log((date.value) - now)
+})
