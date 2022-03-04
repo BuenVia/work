5ca20bf1583c7ed70964825d77e61b65c8c9bf08
SaracensFan <matthewclifford@hotmail.co.uk>
2022-03-04 09:30:41 +0000

    updates

--- language/js/conjugateApp.js
+++ language/js/conjugateApp.js
@@ -5,9 +5,11 @@ const submitBtn = id('submitBtn')
 const answer = id('answer')
 const answerBtn = id('answerBtn')
 const questionEl = id('questionElement')
+const nextBtn = id('nextBtn')
 
 submitBtn.addEventListener('click', init)
 answerBtn.addEventListener('click', checkAnswer)
+nextBtn.addEventListener('click', nextQuestion)
 
 let question
 let number = 0
@@ -15,24 +17,26 @@ let number = 0
 function init() {
     const x = verb.value
     question = words[x]
-    console.log(question.conjugate.length);
     document.querySelector('.choice-container').style.display = 'none'
     document.querySelector('.question-container').style.display = 'block'
     setQuestion()
 }
 
 function setQuestion() {
-    questionEl.innerText = question.conjugate[0].question;
+    questionEl.innerText = question.conjugate[number].question;
 }
 
 function nextQuestion() {
-
+    if (number < question.conjugate.length) {
+        number++
+    }
+    console.log(number);
+    setQuestion()
 }
 
 function checkAnswer() {
-    if (answer.value === question.conjugate[0].answer) {
+    if (answer.value === question.conjugate[number].answer) {
         console.log(true);
-        nextQuestion()
     } else {
         console.log(false);
     }
