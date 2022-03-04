681adaa056629e304bd9b5fde11a21b8f257d837
SaracensFan <matthewclifford@hotmail.co.uk>
2022-03-04 13:42:55 +0000

    update

--- language/js/conjugateApp.js
+++ language/js/conjugateApp.js
@@ -6,6 +6,7 @@ const answer = id('answer')
 const answerBtn = id('answerBtn')
 const questionEl = id('questionElement')
 const nextBtn = id('nextBtn')
+const summaryEl = id('summaryElement')
 
 submitBtn.addEventListener('click', init)
 answerBtn.addEventListener('click', checkAnswer)
@@ -13,32 +14,49 @@ nextBtn.addEventListener('click', nextQuestion)
 
 let question
 let number = 0
+let summary = []
 
 function init() {
     const x = verb.value
     question = words[x]
     document.querySelector('.choice-container').style.display = 'none'
     document.querySelector('.question-container').style.display = 'block'
+    document.querySelector('.next-btn').style.display = 'none'
     setQuestion()
 }
 
 function setQuestion() {
-    questionEl.innerText = question.conjugate[number].question;
+    console.log(number);
+    if (number < question.conjugate.length) {
+        questionEl.innerText = question.conjugate[number].question;
+        document.querySelector('.answer-btn').style.display = 'block'
+        document.querySelector('.next-btn').style.display = 'none'
+        answer.value = ''
+    } else {
+        // Show summary
+        console.log('commplete');
+        document.querySelector('.question-container').style.display = 'none'
+        document.querySelector('.summary-container').style.display = 'block'
+        summaryEl.innerText = summary
+    }
 }
 
 function nextQuestion() {
-    if (number < question.conjugate.length) {
-        number++
-    }
-    console.log(number);
+    number++
     setQuestion()
 }
 
 function checkAnswer() {
     if (answer.value === question.conjugate[number].answer) {
         console.log(true);
+        document.querySelector('.answer-btn').style.display = 'none'
+        document.querySelector('.next-btn').style.display = 'block'
     } else {
+        // Push to array to show in summary
         console.log(false);
+        document.querySelector('.answer-btn').style.display = 'none'
+        document.querySelector('.next-btn').style.display = 'block'
+        summary.push(question.conjugate[number].answer)
     }
 }
 
