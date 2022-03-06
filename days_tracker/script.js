c97d4a3dd1a7324b768105e71e4410936405fe89
SaracensFan <matthewclifford@hotmail.co.uk>
2022-03-06 20:09:04 +0000

    upduate

--- days_tracker/script.js
+++ days_tracker/script.js
@@ -3,6 +3,7 @@ const submitBtn = id('submit')
 const daysEl = id('days')
 const dateRep = id('date-rep')
 const eventName = id('eventName')
+const title = id('title')
 
 const today = new Date();
 let todayYear = today.getFullYear()
@@ -17,28 +18,26 @@ submitBtn.addEventListener('click', operation)
 
 function operation() {
     if(eventName.value === '') {
-        alert('Enter some shit!')
+        alert('Enter something!')
     } else {
         const d1 = new Date(now)
         const d2 = new Date(date.value)
         const diffTime = d2 - d1
         const diffDays = diffTime / (1000 * 3600 * 24)
-        if (diffDays > 0) {
-            console.log(diffDays);
 
-            const format = new Date(date.value)
-            let formatYear = format.getFullYear()
-            let formatMonth = format.getMonth() + 1
-            let formatDay = format.getDate()
-            formatMonth = addZero(formatMonth)
-            formatDay = addZero(formatDay)
-    
-            const formattedDate = `${formatDay}/${formatMonth}/${formatYear}`
-    
-            daysEl.innerText = diffDays
-            dateRep.innerText = `${eventName.value} on ${formattedDate}`
+        const format = new Date(date.value)
+        const dateTwo = `${format.toDateString()}`
+
+        if (diffDays > 0) {
+            document.body.style.background = 'var(--bkgThree)';
+            title.innerText = 'Days Until'
+            dateRep.innerText = `${diffDays} days until ${eventName.value} on ${dateTwo}`
+        } else if (diffDays === 0) {
+            alert('That\'s today!');
         } else {
-            alert('Must be greater than today');
+            document.body.style.background = 'var(--bkgTwo)';
+            title.innerText = 'Days Since'
+            dateRep.innerText = `${Math.abs(diffDays)} days have passed since ${eventName.value} on ${dateTwo}`
         }
     
     }
