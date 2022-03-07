41526bb1ac8cf10fb8923e3dea75361b5ea3940c
SaracensFan <matthewclifford@hotmail.co.uk>
2022-03-07 22:46:15 +0000

    update

--- vocab_test/script.js
+++ vocab_test/script.js
@@ -1,23 +1,66 @@
-const colorsBtn = id('colors')
-const conjunctionsBtn = id('conjunctions')
-const opinionsBtn = id('opinions')
 const englishEl = id('english')
+const spanishEl = id('spanish')
+const selectBtn = id('selectBtn')
+const choice = id('choice')
+const choiceContainer = id('choiceContainer')
+const activityContainer = id('activityContainer')
+const summaryContainer = id('summaryContainer')
+const close = id('close')
+const answerBtn = id('answerBtn')
+const nextBtn = id('nextBtn')
+const resetBtn = id ('resetBtn')
 
 let category
+let questionIndex = 0
 
-colorsBtn.addEventListener('click', loadWords)
-conjunctionsBtn.addEventListener('click', loadWords)
-opinionsBtn.addEventListener('click', loadWords)
+selectBtn.addEventListener('click', loadWords)
+answerBtn.addEventListener('click', checkAnswer)
+nextBtn.addEventListener('click', setQuestion)
+resetBtn.addEventListener('click', reset)
+close.addEventListener('click', reset)
 
 function loadWords() {
-    if (this.id === "colors") category = colors
-    else if (this.id === "conjunctions") category = conjunctions
-    else if (this.id === "opinions") category = opinions
+    if (choice.value === "colors") category = colors
+    else if (choice.value === "conjunctions") category = conjunctions
+    else if (choice.value === "opinions") category = opinions
+    choiceContainer.style.display = 'none'
+    activityContainer.style.display = 'block'
+    close.style.display = 'block'
+    nextBtn.style.display = 'none'
     setQuestion()
 }
 
 function setQuestion() {
-    englishEl.innerText = category[0].english
+    if (questionIndex < category.length) {
+        englishEl.innerText = category[questionIndex].english
+        answerBtn.style.display = 'block'
+        nextBtn.style.display = 'none'
+    } else {
+        activityContainer.style.display = 'none'
+        nextBtn.style.display = 'none'
+        summaryContainer.style.display = 'block'
+        console.log('finished');
+    }
+
+}
+
+function checkAnswer () {
+    answerBtn.style.display = 'none'
+    nextBtn.style.display = 'block'
+    if (spanishEl === category[questionIndex].spanish) {
+        console.log('correct');
+    } else {
+        console.log('incorrect');
+    }
+    questionIndex++
+}
+
+function reset() {
+    category = ''
+    choiceContainer.style.display = 'block'
+    activityContainer.style.display = 'none'
+    summaryContainer.style.display = 'none'
+    close.style.display = 'none'
 }
 
 function id(id) {
