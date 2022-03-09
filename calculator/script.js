fee8f9bae6c5d9b5d982cc3c7e77fc84757857b5
Matthew Clifford <matthewclifford@hotmail.co.uk>
2022-03-09 12:26:07 +0000

    update

--- calculator/script.js
+++ calculator/script.js
@@ -1,39 +1,40 @@
-// const zero = document.getElementById('zero')
-const one = id('one')
-const two = id('two')
-const three = id('three')
-// const four = document.getElementById('four')
-// const five = document.getElementById('five')
-// const six = document.getElementById('six')
-// const seven = document.getElementById('seven')
-// const eight = document.getElementById('eight')
-// const nine = document.getElementById('nine')
-const plus = id('plus')
-const equals = id('equals')
-
-let userInput = []
-
-one.addEventListener('click', operation)
-two.addEventListener('click', operation)
-three.addEventListener('click', operation)
-plus.addEventListener('click', operation)
-equals.addEventListener('click', output)
-
-function operation() {
-    console.log(this.value);
-    if (one || two) {
-        userInput += this.value
-    } else if (plus) {
-        
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
     }
 
+    clear() {
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
+    }
+
+    delete() {
+
+    }
+
+    appendNumber(number) {
+
+    }
+
+    chooseOperation(operation) {
+
+    }
+
+    compute() {
+
+    }
+
+    updateDisplay() {
+
+    }
 }
 
-function output() {
-    console.log(userInput);
-}
-
-//Helper Function
-function id(id) {
-    return document.getElementById(id)
-}
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const eqaulsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
