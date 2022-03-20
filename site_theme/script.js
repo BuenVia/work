bffd646af02bdf32b237a02be079f3c674df30ca
SaracensFan <matthewclifford@hotmail.co.uk>
2022-03-20 08:42:51 +0000

    updatw

--- site_theme/script.js
+++ site_theme/script.js
@@ -1,12 +1,15 @@
 const tabOne = id('tabOne')
+const tabTwo = id('tabTwo')
 const sectionOne = id('sectionOne')
+const sectionTwo = id('sectionTwo')
 
-tabOne.addEventListener('click', () => {
-    console.log(true);
-})
+tabOne.addEventListener('click', changeTab)
+tabTwo.addEventListener('click', changeTab)
 
 function changeTab() {
-    
+    if (sectionOne.style.display !== 'block') {
+    console.log('tab one');
+    }
 }
 
 function id(id) {
