4a1d526d752cd18e4650cf58922a116b082dc903
SaracensFan <matthewclifford@hotmail.co.uk>
2022-03-23 08:02:56 +0000

    dark mode

--- site_theme/script.js
+++ site_theme/script.js
@@ -16,38 +16,23 @@ styleBtn.addEventListener('click', changeStyle)
 
 //Change Tab Function
 function changeTab(sectionName) {
-   let i, section, btnLink
-   section = document.getElementsByClassName('section')
-   for (i = 0; i < section.length; i++) {
+   const section = document.getElementsByClassName('section')
+   for (let i = 0; i < section.length; i++) {
        section[i].style.display = 'none'
    }
-   btnLink = document.getElementsByClassName('btn-link')
-   for (i = 0; i < btnLink.length; i++) {
-       btnLink[i].style.backgroundColor = ''
-   }
-    document.getElementById(sectionName).style.display = 'block'
+   id(sectionName).style.display = 'block'
 }
 
 id('tabOne').click()
 
 // Change Style Function
 function changeStyle() {
-    header.classList.toggle('head-dark')
-    footer.classList.toggle('footer-dark')
     if (styleBtn.innerText === 'Dark') {
+        document.body.classList.add('dark-mode')
         styleBtn.innerText = 'Light'
-        navBar.style.backgroundColor = '#333'
-        side.style.backgroundColor = '#111'
-        side.style.color = '#ddd'
-        mainCont.style.backgroundColor = '#222'
-        mainCont.style.color = '#ddd'
     } else {
+        document.body.classList.remove('dark-mode')
         styleBtn.innerText = 'Dark'
-        navBar.style.backgroundColor = ''
-        side.style.backgroundColor = ''
-        side.style.color = ''
-        mainCont.style.backgroundColor = ''
-        mainCont.style.color = ''
     }
 }
 
