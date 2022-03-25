b73ee3a1f931beeb78282de13835c401f03991ec
Matthew Clifford <matthewclifford@hotmail.co.uk>
2022-03-25 12:01:31 +0000

    update

--- quiz/script.js
+++ quiz/script.js
@@ -4,7 +4,27 @@
 
 //Summary
 
-
-const questions = {
-    
-}
+//Questions
+const questions = [
+    {
+        question: "What color is rgb(0, 0, 0)?",
+        answers: [
+            { text: "black", correct: true },
+            { text: "white", correct: false }
+        ]
+    },
+    {
+        question: "What has six strings?",
+        answers: [
+            { text: "guitar", correct: true },
+            { text: "drums", correct: false }
+        ]
+    },
+    {
+        question: "What year is it?",
+        answers: [
+            { text: "2022", correct: true },
+            { text: "1902", correct: false }
+        ]
+    },
+]
