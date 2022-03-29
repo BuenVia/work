609040bd3f7b93cec917808d02d8eb911878352b
SaracensFan <matthewclifford@hotmail.co.uk>
2022-03-29 21:21:49 +0100

    update

--- quiz/script.js
+++ quiz/script.js
@@ -1,19 +1,43 @@
 const startBtn = id('startBtn')
+const question = id('question')
+const ansOne = id('ansOne')
+const ansTwo = id('ansTwo')
+const ansBtnOne = id('ansBtnOne')
+const ansBtnTwo = id('ansBtnTwo')
+
+let questionIndex = 0
 
 startBtn.addEventListener('click', init)
+
+
 //Start quiz
 function init() {
     query('.intro').style.display = 'none'
     query('.quiz-card').style.display = 'flex'
-    setNextQuestion()
-}
-
-function setNextQuestion() {
-    
+    shuffle(questions)
+    setQuestion(questions)
 }
 
 //Quiz Functionality
+function setQuestion() {
+    if (questionIndex < questions.length) {
+        question.innerText = questions[questionIndex].question
+    }
+}
 
+function checkAnswer() {
+
+}
+
+function shuffle(array) {
+    let currentIndex = array.length, randomIndex
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex)
+        currentIndex--
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]]
+    }
+    return array
+}
 //Summary
 
 //Helper Functions
